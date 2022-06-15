6eac9ab399e9025f290017c0bc60d708b183e849
Seth Wagener <sethwagener@gmail.com>
2022-06-15 20:49:05 +1200

    5x5 view grid.

--- Form1.cs
+++ Form1.cs
@@ -15,18 +15,23 @@ namespace Grid_based_map
     public partial class Form1 : Form
     {
         Graphics g;
-        Rectangle[] Tile = new Rectangle[9];
+        Rectangle[] Tile = new Rectangle[25];
         int TileID = 0;
-        int[,,] Tiles = new int[5, 5, 2]
+        int[,,] Tiles = new int[10, 10, 2]
             {
-                { {2,3},{2,3},{2, 5},{2,7},{2,10} },
-                { {2,2},{2,2},{1,7},{2,8},{2,321} },
-                { {1,4},{2,1},{1,2},{2,543},{2,6546} },
-                { {1,6},{1,8},{1,9},{1,546},{2,234} },
-                { {1,3},{1,2},{1,5},{1,1254},{1,615} }
+                { {1,3},{1,3},{2,5},{2,7},{2,1}, {2,3},{2,3},{2,5},{2,7},{2,1}  },
+                { {1,2},{1,2},{1,7},{2,8},{2,6}, {2,2},{2,2},{1,7},{2,8},{2,6}  },
+                { {1,4},{2,1},{1,2},{2,6},{2,3}, {1,4},{2,1},{1,2},{2,6},{2,3}  },
+                { {1,6},{1,8},{1,9},{1,5},{2,7}, {1,6},{1,8},{1,9},{1,5},{2,7}  },
+                { {1,3},{1,2},{1,5},{1,9},{1,5}, {1,3},{1,2},{1,5},{1,9},{1,5}  },
+                { {2,3},{2,3},{2,5},{2,7},{2,1}, {2,3},{2,3},{2,5},{2,7},{2,1}  },
+                { {2,2},{2,2},{1,7},{2,8},{2,6}, {2,2},{2,2},{1,7},{2,8},{2,6}  },
+                { {1,4},{2,1},{1,2},{2,6},{2,3}, {1,4},{2,1},{1,2},{2,6},{2,3}  },
+                { {1,6},{1,8},{1,9},{1,5},{2,7}, {1,6},{1,8},{1,9},{1,5},{2,7}  },
+                { {1,3},{1,2},{1,5},{1,9},{1,5}, {1,3},{1,2},{1,5},{1,9},{1,5}  }
             };
-        int[,] ViewRange = new int[3, 3];
-        int tileX=2, tileY=2;
+        int[,] ViewRange = new int[5, 5];
+        int tileX=5, tileY=5;
         Brush Grass = Brushes.Green;
         Brush Water = Brushes.Blue;
         public Form1()
@@ -38,26 +43,26 @@ namespace Grid_based_map
 
         public void DrawGrid()
         {
-            for (int i = 0; i < 9; i++)
+            for (int i = 0; i < 25; i++)
             {
                 Tile[i] = Rectangle.Empty;
             }
             TileID = 0;
             //setting up rectangles for grid
-            for (int h = 0; h < 3; h++)
+            for (int h = 0; h < 5; h++)
             {
-                for (int w = 0; w < 3; w++)
+                for (int w = 0; w < 5; w++)
                 {
-                    Tile[TileID] = new Rectangle(0 + (200 * w), 0 + (200 * h), 200, 200);
+                    Tile[TileID] = new Rectangle(0 + (120 * w), 0 + (120 * h), 120, 120);
                     TileID++;
                 }
             }
 
-            for (int Y = -1; Y < 2; Y++)
+            for (int Y = -2; Y < 3; Y++)
             {
-                for (int X = -1; X < 2; X++)
+                for (int X = -2; X < 3; X++)
                 {
-                    ViewRange[X+1, Y+1] = Tiles[tileY + X, tileX + Y, 0];
+                    ViewRange[X+2, Y+2] = Tiles[tileY + X, tileX + Y, 0];
                 }
             }
             panel1.Invalidate();
@@ -65,19 +70,19 @@ namespace Grid_based_map
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyData == Keys.Left && tileX>1)
+            if (e.KeyData == Keys.Left && tileX>2)
             {
                 tileX--;
             }
-            if (e.KeyData == Keys.Right && tileX < 3)
+            if (e.KeyData == Keys.Right && tileX < 7)
             {
                 tileX++;
             }
-            if (e.KeyData == Keys.Up && tileY > 1)
+            if (e.KeyData == Keys.Up && tileY > 2)
             {
                 tileY--;
             }
-            if (e.KeyData == Keys.Down && tileY < 3)
+            if (e.KeyData == Keys.Down && tileY < 7)
             {
                 tileY++;
             }
@@ -88,9 +93,9 @@ namespace Grid_based_map
         {
             g = e.Graphics;
             TileID = 0;
-            for (int h = 0; h < 3; h++)
+            for (int h = 0; h < 5; h++)
             {
-                for (int w = 0; w < 3; w++)
+                for (int w = 0; w < 5; w++)
                 {
                     if (ViewRange[h,w]==1)
                     {
