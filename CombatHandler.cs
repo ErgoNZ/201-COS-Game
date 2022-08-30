4d77bd859550ac0a01b483f8a7b1033984aae5e6
ErgoNZ <69284626+ErgoNZ@users.noreply.github.com>
2022-08-30 15:05:15 +1200

    Continued work on CombatHandler.

--- CombatHandler.cs
+++ CombatHandler.cs
@@ -9,10 +9,24 @@ namespace Grid_based_map
         List<string> EncounterTable = new List<string>();
         List<Tuple<double,string, string, string>> EncounterData = new List<Tuple<double,string, string, string>>();
         List<Tuple<double, string, string, string>> SelEncounters = new List<Tuple<double, string, string, string>>();
+        //Tuple(Hp,Atk,Def,Spd,Crit,Element,Position)
+        List<Tuple<int,int,int,int,int,string,int>> CurrentEncounter = new List<Tuple<int,int,int,int,int,string,int>>();
         Random Roll = new Random();
         public void EncounterListSetup(int LevelData)
         {
+            EncounterTable.Clear();
+            EncounterData.Clear();
             using (StreamReader FileReader = new StreamReader("../../../CombatData/EncounterTables/" + LevelData + ".txt"))
+            {
+               string line;
+               while ((line = FileReader.ReadLine()) != null)
+               {
+                 EncounterTable.Add(line);
+               }
+            }
+         foreach(string FightData in EncounterTable)
+         {
+                using (StreamReader FileReader = new StreamReader("../../../CombatData/EncounterTables/" + FightData + ".txt"))
             {
                 string line, Foe1, Foe2, Foe3;
                 double Tile;
@@ -20,6 +34,10 @@ namespace Grid_based_map
 
                 while ((line = FileReader.ReadLine()) != null)
                 {
+                    if (count == 4)
+                    {
+                        EncounterData.Add(Tuple<double,string,string,string>(Tile,Foe1,Foe2,Foe3));
+                    }
                     if (count == 0)
                     {
                         Tile = double.Parse(line);
@@ -39,9 +57,11 @@ namespace Grid_based_map
                     count++;
                 }
             }
+         }
         }
         public void EncounterRoll(int TileData)
         {          
+            SelEncounters.Clear();
             if (Roll.Next(0, 101) >= 90)
             {
                 foreach(Tuple<double,string,string,string> tuple in EncounterData)
@@ -52,7 +72,10 @@ namespace Grid_based_map
                     }
                 }
                 Roll.Next(0,SelEncounters.Count+1);
-                
+                for(int i; i<3;i++;)
+                {
+                  //Enemy data is read here from their files
+                }
             }
         }
     }
